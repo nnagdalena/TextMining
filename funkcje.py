c2d767f9b34229ad00ed054547877a7830957b2a
Magda Bogacz <foxueen@gmail.com>
2022-03-27 11:34:47 +0200

    upload wordcloud

---
+++ funkcje.py
@@ -0,0 +1,27 @@
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+stop_words = stopwords.words("english")
+
+
+def broom(x: str):
+    x = re.sub('([;:]+[)(><-]+)', ' ', x)
+    x = x.lower()
+    x = re.sub(r'\d', ' ', x)
+    x = re.sub('<[^>]+>', ' ', x)
+    x = re.sub(r'[^\w\s]+', ' ', x)
+    x = re.sub(r'\s{2,}', ' ', x)
+    return x
+
+
+def usun_stopwords(x: str) -> list:
+    lista = x.split(' ')
+    nowalista: list = []
+    [nowalista.append(word) for word in lista if word not in stop_words]
+    return nowalista
+
+
+def stemming(lista: list):
+    porter = PorterStemmer()
+    return [porter.stem(x) for x in lista]
---
+++ main.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from wordcloud import WordCloud
+from funkcje import broom, usun_stopwords, stemming, stop_words
+
+fake = pd.read_csv('News_dataset/Fake.csv')
+true = pd.read_csv('News_dataset/True.csv')
+
+list = []
+
+fake_title = " ".join(x for x in fake.title)
+list.append(fake_title)
+true_title = " ".join(x for x in true.title)
+list.append(true_title)
+# fake_text = " ".join(x for x in fake.text)
+# list.append(fake_text)
+# true_text = " ".join(x for x in true.text)
+# list.append(true_text)
+
+for ziomek in list:
+    ziomek = broom(ziomek)
+    ziomek = usun_stopwords(ziomek)
+    ziomek = stemming(ziomek)
+    ziomek = " ".join(ziomek)
+
+fake_title_wc = WordCloud(stopwords=stop_words, background_color="white").generate(fake_title)
+true_title_wc = WordCloud(stopwords=stop_words, background_color="white").generate(true_title)
+# fake_text_wc = WordCloud(stopwords=stop_words, background_color="white").generate(fake_title)
+# true_text_wc = WordCloud(stopwords=stop_words, background_color="white").generate(true_title)
+
+lista_wc = [fake_title_wc, true_title_wc]
+
+for wc in lista_wc:
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
